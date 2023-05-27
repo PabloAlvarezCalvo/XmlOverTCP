2e4f07c3f653d663d2122b5471d2a496df5d1392
PabloAlvarezCalvo <PabloAlvarezCalvo@gmail.com>
2023-05-27 17:29:01 +0200

    tcpConnection method

--- src/main/java/client/XMLtoTCP.java
+++ src/main/java/client/XMLtoTCP.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -14,17 +15,32 @@ public class XMLtoTCP {
     private final static String XML_BODY = "<Msg Name=\"OpenStudies\" Type=\"XA\"><Param Name=\"ProcessId\">Identificador</Param></Msg>";
     private final static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         InetAddress serverAddress = inputIP();
-        int port = inputPort();
+        int serverPort = inputPort();
         String xmlString = inputPID();
 
+        tcpConnection(serverAddress, serverPort, xmlString);
+    }
+
+    private static void tcpConnection(InetAddress serverAddress, int serverPort, String xmlString) {
         try(
-                Socket clientSocket = new Socket(serverAddress, port);
-                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream())
+                Socket clientSocket = new Socket(serverAddress, serverPort);
+                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+                DataInputStream dis = new DataInputStream(clientSocket.getInputStream())
         ){
+            System.out.println("Sending:");
+            System.out.println(xmlString + "\n");
+
             dos.writeUTF(xmlString);
+
+            System.out.println("Server response:");
+            System.out.println(dis.readUTF());
+
+
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 
@@ -55,17 +71,23 @@ public class XMLtoTCP {
     }
 
     private static int inputPort() {
-        int port;
+        int port = -1;
         boolean validPort = false;
 
 
         System.out.println("Specify the port to connecto to:");
         do {
-            port = Integer.parseInt(sc.nextLine());
+            try {
+                port = Integer.parseInt(sc.nextLine());
 
-            if (port > 0 && port < 49151) {
-                validPort = true;
-            } else System.out.println("Port out of range (1 - 49151), please try again:");
+                if (port > 0 && port < 49151) {
+                    validPort = true;
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
+            if (!validPort) System.out.println("Port out of range (1 - 49151), please try again:");
 
         } while (!validPort);
 
