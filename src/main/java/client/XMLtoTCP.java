ee533d5ab7ede2eb99ad14d4e71ccea2a6553fce
PabloAlvarezCalvo <PabloAlvarezCalvo@gmail.com>
2023-05-27 14:54:28 +0200

    Client

---
+++ src/main/java/client/XMLtoTCP.java
@@ -0,0 +1,12 @@
+package client;
+
+public class XMLtoTCP {
+    private final static String XML_TEMPLATE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Msg Name=\"OpenStudies\" Type=\"XA\"><Param Name=\"ProcessId\">Identificador</Param></Msg>";
+    public static void main(String[] args) {
+
+
+        do {
+
+        } while ();
+    }
+}
